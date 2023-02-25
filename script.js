5daed243fef9c2d76dd7cb87e117047bc269f782
Roberto Meré <121988408+RogerEaux@users.noreply.github.com>
2023-02-25 01:10:04 -0600

    Change book constructor arg to an array

--- script.js
+++ script.js
@@ -1,18 +1,52 @@
 const library = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+function Book(bookInfo) {
+  console.log(bookInfo);
+  this.title = bookInfo[0];
+  this.author = bookInfo[1];
+  this.pages = bookInfo[2];
+  this.read = false;
+  if (bookInfo[3]) {
+    this.read = true;
+  }
 }
 
-const modal = document.querySelector('.modal');
-const addBook = document.querySelector('.add-book button');
+function popUp() {
+  const modal = document.querySelector('.modal');
+  const addBook = document.querySelector('.add-book button');
 
-addBook.addEventListener('click', () => (modal.style.visibility = 'visible'));
-window.addEventListener('click', (event) => {
-  if (event.target === modal) {
-    modal.style.visibility = 'hidden';
+  addBook.addEventListener('click', () => (modal.style.visibility = 'visible'));
+  window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+      modal.style.visibility = 'hidden';
+    }
+  });
+}
+
+function getData(form) {
+  const formData = new FormData(form);
+  const bookInfo = [];
+
+  for (let pair of formData.entries()) {
+    bookInfo.push(pair[1]);
   }
-});
+
+  return bookInfo;
+}
+
+function submit() {
+  const form = document.querySelector('.new-book-form');
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    let book = new Book(getData(event.target));
+    console.log(book);
+  });
+}
+
+function setUp() {
+  popUp();
+  submit();
+}
+
+setUp();
